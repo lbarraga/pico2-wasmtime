a2b570abdba2827d8c861527340eeec61aab8563
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2025-12-26 18:30:44 +0100

    non wasmtime working

---
+++ led/src/main.rs
@@ -0,0 +1,46 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_rp as hal;
+use embassy_rp::block::ImageDef;
+use embassy_rp::gpio::{Level, Output};
+use embassy_time::Timer;
+
+//Panic Handler
+use panic_probe as _;
+// Defmt Logging
+use defmt_rtt as _;
+
+/// Tell the Boot ROM about our application
+#[unsafe(link_section = ".start_block")]
+#[used]
+pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let mut led = Output::new(p.PIN_13, Level::Low);
+
+    loop {
+        led.set_high(); // Turn on the LED
+        Timer::after_millis(500).await;
+
+        led.set_low(); // Turn off the LED
+        Timer::after_millis(500).await;
+    }
+}
+
+// Program metadata for `picotool info`.
+// This isn't needed, but it's recomended to have these minimal entries.
+#[unsafe(link_section = ".bi_entries")]
+#[used]
+pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
+    embassy_rp::binary_info::rp_program_name!(c"external-led"),
+    embassy_rp::binary_info::rp_program_description!(c"your program description"),
+    embassy_rp::binary_info::rp_cargo_version!(),
+    embassy_rp::binary_info::rp_program_build_attribute!(),
+];
+
+// End of file
